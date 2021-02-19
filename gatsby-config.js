6bf91dfd8e5c1f8f59ff4f2a8804bbb7fa1eee69
Timothy Brantley II <tbrantleyii@gmail.com>
2021-02-19 16:38:08 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "casecheck-frontend-test",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
