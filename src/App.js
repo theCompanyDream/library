4d021d06820706d6638485a84ac6b4f960e31169
Timothy Brantley II <tbrantleyii@gmail.com>
2021-03-05 12:09:20 -0500

    fix: now we have a spa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState} from "react"
+import React, { Suspense, useEffect, useState} from "react"
 
 import {
-  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom"
@@ -15,24 +14,14 @@ const Detail = React.lazy(() => import('./components/detail'))
 const Map = React.lazy(() => import('./components/map'))
 const Search = React.lazy(() => import('./components/search'))
 
-const LazyComponent = ({ Component, ...props }) => (
-  <React.Suspense fallback={FallBackComp}>
-    <Component {...props} /> 
-  </React.Suspense>
-)
-
-const FallBackComp = (
-  <p>Loading ...</p>
-)
-
-const IndexPage = ({ data }) => {
+const App = () => {
 
   const [body, setState] = useState([])
   const [components, setComponents] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
-      await axios.get(`https://data.cityofchicago.org/resource/cwig-ma7x.json`).then( res => {
+      await axios.get(`https://www.googleapis.com/books/v1/volumes?q=kaplan%20test%20prep`).then( res => {
         const payload = res.data;
         setState(payload)
       });
@@ -42,28 +31,18 @@ const IndexPage = ({ data }) => {
   }, [])
 
   return (
-      <Router>
-        <Layout>
-          <Switch>
-              <Route path="/">
-                <Home path="/" />
-              </Route>
+    <Layout>
+      <Suspense fallback={<div>Loading ...</div>}>
+          <Route exact path="/" component={Home} />
 
-              <Route path="/search">
-                <LazyComponent Component={Search} path="search" />
-              </Route>
+          <Route path="/search" component={Search} />
 
-              <Route path="/detail">
-                <LazyComponent Component={Detail} path="detail" />
-              </Route>
+          <Route path="/detail"  component={Detail} />
 
-              <Route path="/map">
-                <LazyComponent Component={Map} path="map" />
-              </Route>
-          </Switch>
-        </Layout>
-      </Router>
+          <Route path="/map" component={Map} />
+      </Suspense>
+    </Layout>
   )
 }
 
-export default IndexPage
+export default App
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,14 +4,20 @@ import "./styles/base.scss"
 const Layout = ({ children }) => (
 	<div className="container">
 		<nav className="navbar" role="navigation" aria-label="main navigation">
-			<div className="navbar-item">
-				<a className="navbar-item" href="https://www.casechek.com/wp-content/themes/casechek/images/casechek-logo.png">
-					<img src="https://www.casechek.com/wp-content/themes/casechek/images/casechek-logo.png" width="112" height="28" />
+			<div className="navbar-start">
+				<a className="navbar-item" to="/">
+					<img src="https://kaplan.com/wp-content/themes/kaplan/images/kaplan-logo.png?12345" width="112" height="28" />
 				</a>
 			</div>
 
 			<div class="navbar-end">
-				Frontend Test
+				<div className="navbar-item">
+					<div className="buttons">
+						<a className="button is-primary" href="#">
+							Frontend Test
+						</a>
+					</div>
+				</div>
 			</div>
 		</nav>
 		{children}
